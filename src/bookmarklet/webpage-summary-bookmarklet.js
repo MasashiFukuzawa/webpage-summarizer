7ac6c2d7f42042acafe8317af98fc9a6329fd3e4
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2023-05-04 22:25:55 +0900

    feat: implement markdown-convertor

--- src/bookmarklet/webpage-summary-bookmarklet.js
+++ src/bookmarklet/webpage-summary-bookmarklet.js
@@ -1,12 +1,34 @@
-(function () {
+(async function () {
+  const html = document.documentElement.innerHTML;
+  const convertedText = await fetch('http://127.0.0.1:51679', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: html,
+  });
+  const summary = await fetch('http://127.0.0.1:51696', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: convertedText,
+  });
   const w = window.open('', '_blank');
   w.document.write(
-    '<html><head><title>Summary by ChatGPT</title></head><body><pre>' +
-      document.documentElement.innerHTML
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;') +
-      '</pre></body></html>'
+    `<html>
+      <head>
+        <title>Summary by ChatGPT</title>
+      </head>
+      <body>
+        <pre>
+          ${summary
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')}
+        </pre>
+      </body>
+    </html>`
   );
   w.document.close();
 })();
--- src/html-parser/src/index.ts
+++ src/html-parser/src/index.ts
@@ -1,7 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import convertToMarkdown from './markdown-convertor';
+import trimHtml from './trimming';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.text('Hello Hono!'))
+// GET is appropriate, but POST is used
+// because of the possibility of huge HTML being crossed.
+app.post('/', async (c) => {
+  const html = await c.req.text();
 
-export default app
+  // Trimmed to reduce the number of tokens (cost)
+  // before passing to the ChatGPT API,
+  // and to remove as much meaningless data as possible
+  const trimmedHtml = trimHtml(html);
+
+  // HTML tags are converted to Markdown
+  // to further compress the character count.
+  // Note, however, that this is a rough markdown.
+  const markdown = convertToMarkdown(trimmedHtml);
+
+  return c.text(markdown);
+});
+
+export default app;
